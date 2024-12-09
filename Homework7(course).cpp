1c52c4c491ea8c5e4617f50e3c29118676e07369
lilia <lilia@DESKTOP-64J80RS>
2024-12-09 03:01:09 +0200

    Добавьте файлы проекта.

---
+++ Homework7(course).cpp
@@ -0,0 +1,68 @@
+﻿/*2)
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int mas[10][10];
+
+    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            mas[i][j] = 0;
+        }
+    }
+
+  
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << mas[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}*/
+/*3)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for (int i = 100; i <= 200; i += 2) {
+        cout << i << " \n";
+    }
+
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+/*4)
+int main() {
+    int i = 100;  
+
+    
+    while (i <= 200) {
+        cout << i << " \n";
+        i += 2;  
+    }
+
+    return 0;
+}*/
+/*5)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 100;  
+
+    do {
+        cout << i << " \n";
+        i += 2;  
+    } while (i <= 200);
+
+    return 0;
+}
+*/
